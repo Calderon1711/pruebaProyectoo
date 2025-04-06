175ad61378a596fe797ec5610c7ebeca48bd84ce
Calderon1711 <alejandro.calderon.campos@est.una.ac.cr>
2025-04-06 14:13:17 -0600

    Agregada la clase Persona

---
+++ Persona.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aleca on 4/6/2025.
+//
+
+#include "Persona.h"
---
+++ Persona.h
@@ -0,0 +1,21 @@
+//
+// Created by aleca on 4/6/2025.
+//
+
+#ifndef PERSONA_H
+#define PERSONA_H
+using namespace std;
+#include <iostream>
+
+
+class Persona {
+private:
+  int id;
+  string name;
+  public:
+    Persona();
+};
+
+
+
+#endif //PERSONA_H
